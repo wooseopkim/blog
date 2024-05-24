473f7700247f265575114c7fee890aaf68f7f51f
Wooseop Kim <wooseop@protonmail.com>
2024-05-25 02:15:32 +0900

    Format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   site: 'https://blog.wooseop.kim',
-  integrations: [
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap()],
   compressHTML: true,
 });
