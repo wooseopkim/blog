d9e401751f8fab564960d4cff4a7eaa901b28302
Wooseop Kim <wooseop@protonmail.com>
2025-01-01 22:35:57 +0900

    Update title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '花無十日紅';
-export const SITE_DESCRIPTION = '하지만 십 년을 가는 고통은 있습니다.';
+export const SITE_TITLE = '一切唯心造';
+export const SITE_DESCRIPTION = '검은 네모 사이의 회색 점처럼';
