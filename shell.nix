bc94202c04767e1b3634c79ae07afbdebc6a8b0d
Wooseop Kim <wooseop@protonmail.com>
2025-02-12 23:28:18 +0900

    Get Nix shell working

--- shell.nix
+++ shell.nix
@@ -1,13 +1,6 @@
 let
-  nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/00769b0532199db4e1bda59865f00f3a86232c75.tar.gz";
-    sha256 = "1bs1mvzhba0rpm49zsn3rxikh0zgc7s2z7nbc4c7ldr0pxb6mxxq";
-  };
+  pkgs = import <nixpkgs> { };
 in
-{
-  pkgs ? import nixpkgs { },
-  ...
-}:
 with pkgs;
 mkShellNoCC {
   packages = [
